dc8b217731693a18f8fc350f3f701cb27c999188
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-02-03 12:07:59 +0100

    Create get-languages.php

---
+++ api/get-languages.php
@@ -0,0 +1,45 @@
+<?php
+header("Content-Type: application/json");
+echo json_encode([
+    [
+        "name" => "日本語",
+        "value" => "1"
+    ],
+    [
+        "name" => "Rōmaji",
+        "value" => "2"
+    ],
+    [
+        "name" => "한국어",
+        "value" => "3"
+    ],
+    [
+        "name" => "中国人",
+        "value" => "4"
+    ],
+    [
+        "name" => "Français",
+        "value" => "5"
+    ],
+    [
+        "name" => "Deutsch",
+        "value" => "6"
+    ],
+    [
+        "name" => "Español",
+        "value" => "7"
+    ],
+    [
+        "name" => "italiano",
+        "value" => "8"
+    ],
+    [
+        "name" => "English",
+        "value" => "9"
+    ],
+    [
+        "name" => "čeština",
+        "value" => "10"
+    ]
+]);
+?>
