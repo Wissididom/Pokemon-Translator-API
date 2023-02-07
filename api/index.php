d2b8a1275300d3510b62ff8ca65580470586c48d
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-02-07 14:43:45 +0100

    Add additional optional outlang GET parameter (#4)
    Tested by opening the pages in a browser on Vercel.
    
    Works as expected LGTM 👍

--- api/index.php
+++ api/index.php
@@ -1,4 +1,201 @@
 <?php
+$nameFoundPatterns = [
+    "1" => [ // Japanese Hiragana/Katakana Pokémon
+        "1" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no nihongo wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 일본어이름은 \"%s\" 입니다.", // Korean
+        "4" => "您的神奇寶貝的日語名稱是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom japonais de votre Pokémon est \"%s\".", // French
+        "6" => "Der japanische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre japonés de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome giapponese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Japanese name is \"%s\".", // English
+        "10" => "Japonské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的日本名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome japonês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "2" => [ // Japanese Rōmaji Pokémon
+        "1" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no nihongo wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 일본어이름은 \"%s\" 입니다.", // Korean
+        "4" => "您的神奇寶貝的日語名稱是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom japonais de votre Pokémon est \"%s\".", // French
+        "6" => "Der japanische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre japonés de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome giapponese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Japanese name is \"%s\".", // English
+        "10" => "Japonské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的日本名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome japonês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "3" => [ // Korean Pokémon
+        "1" => "あなたのポケモンの韓国名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Kankoku-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "당신의 포켓몬의 한국 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的韓國名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom coréen de votre Pokémon est \"%s\".", // French
+        "6" => "Der koreanische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre coreano de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome coreano del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Korean name is \"%s\".", // English
+        "10" => "Korejské jméno vašeho pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの韓国名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的韩国名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome coreano do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "4" => [ // Chinese (traditional) Pokémon
+        "1" => "あなたのポケモンの中国名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Chūgoku-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 중국 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的寶可夢的中文名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom chinois de votre Pokémon est \"%s\".", // French
+        "6" => "Der chinesische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre chino de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome cinese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Chinese name is \"%s\".", // English
+        "10" => "Čínské jméno vašeho pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの中国名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的宝可梦的中文名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome chinês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "5" => [ // French Pokémon
+        "1" => "あなたのポケモンのフランス語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no furansugo-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 프랑스어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的法語名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom français de votre Pokémon est \"%s\".", // French
+        "6" => "Der französische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre francés de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome francese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's French name is \"%s\".", // English
+        "10" => "Francouzské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの中国名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的法语名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome francês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "6" => [ // German Pokémon
+        "1" => "あなたのポケモンのドイツ語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no doitsugo-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 독일어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的德語名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom allemand de votre Pokémon est \"%s\".", // French
+        "6" => "Der deutsche Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre alemán de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome tedesco del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's German name is \"%s\".", // English
+        "10" => "Německé jméno vašeho pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンのドイツ語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的德语名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome alemão do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "7" => [ // Spanish Pokémon
+        "1" => "あなたのポケモンのスペイン語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Supeingo-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 스페인어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的西班牙名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom espagnol de votre Pokémon est \"%s\".", // French
+        "6" => "Der spanische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre en español de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome spagnolo del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Spanish name is \"%s\".", // English
+        "10" => "Španělské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンのスペイン語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的西班牙名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome espanhol do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "8" => [ // Italian Pokémon
+        "1" => "あなたのポケモンのイタリア語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Itaria katarina wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 이탈리아어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的意大利名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom italien de votre Pokémon est \"%s\".", // French
+        "6" => "Der italienische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre italiano de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome italiano del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Italian name is \"%s\".", // English
+        "10" => "Italské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンのイタリア語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的意大利名字是\"%s\"。", // Chinese (simplified)
+        "13" => "O nome italiano do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "9" => [ // English Pokémon
+        "1" => "あなたのポケモンの英語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no eigo-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 영어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的英文名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom anglais de votre Pokémon est \"%s\".", // French
+        "6" => "Der englische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre en inglés de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome inglese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's English name is \"%s\".", // English
+        "10" => "Anglické jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの英語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的英文名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome em inglês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "10" => [ // Czech Pokémon
+        "1" => "あなたのポケモンのチェコ語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Cheko katarina wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 체코 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的捷克名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom tchèque de votre Pokémon est \"%s\".", // French
+        "6" => "Der tschechische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre checo de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome ceco del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Czech name is \"%s\".", // English
+        "10" => "Váš pokémon se česky jmenuje \"%s\".", // Czech
+        "11" => "あなたのポケモンのチェコ語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的捷克名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome tcheco do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "11" => [ // Japanese Hiragana/Katakana Pokémon
+        "1" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no nihongo wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 일본어이름은 \"%s\" 입니다.", // Korean
+        "4" => "您的神奇寶貝的日語名稱是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom japonais de votre Pokémon est \"%s\".", // French
+        "6" => "Der japanische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre japonés de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome giapponese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Japanese name is \"%s\".", // English
+        "10" => "Japonské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの日本語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的日本名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome japonês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "12" => [ // Chinese (simplified) Pokémon
+        "1" => "あなたのポケモンの中国名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Chūgoku-mei wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 중국 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的寶可夢的中文名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom chinois de votre Pokémon est \"%s\".", // French
+        "6" => "Der chinesische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre chino de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome cinese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Chinese name is \"%s\".", // English
+        "10" => "Čínské jméno vašeho pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンの中国名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的宝可梦的中文名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome chinês do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ],
+    "13" => [ // (Brazilian) Portuguese Pokémon
+        "1" => "あなたのポケモンのポルトガル語名は「%s」です。", // Japanese Hiragana/Katakana
+        "2" => "Anata no pokemon no Porutogaru katarina wa \"%s\" desu.", // Japanese Rōmaji
+        "3" => "포켓몬의 포르투갈어 이름은 \"%s\" 입니다.", // Korean
+        "4" => "你的口袋妖怪的葡萄牙語名字是 \"%s\"。", // Chinese (traditional)
+        "5" => "Le nom portugais de votre Pokémon est \"%s\".", // French
+        "6" => "Der portugiesische Name deines Pokemons lautet \"%s\".", // German
+        "7" => "El nombre portugués de tu Pokémon es \"%s\".", // Spanish
+        "8" => "Il nome portoghese del tuo Pokemon è \"%s\".", // Italian
+        "9" => "Your Pokemon's Portuguese name is \"%s\".", // English
+        "10" => "Portugalské jméno vašeho Pokémona je \"%s\".", // Czech
+        "11" => "あなたのポケモンのポルトガル語名は「%s」です。", // Japanese Hiragana/Katakana
+        "12" => "你的口袋妖怪的葡萄牙语名字是 \"%s\"。", // Chinese (simplified)
+        "13" => "O nome em português do seu Pokémon é \"%s\"." // (Brazilian) Portuguese
+    ]
+];
 function csv2json($csv) {
     $data = array_map("str_getcsv", explode("\n", $csv));
     $columns = $data[0];
@@ -20,48 +217,59 @@ if ($language > 10 || $language < 1) {
     echo "400 Bad Request - Invalid Language";
     die();
 }
+$outlang = $language;
+if (isset($_GET['outlang']))
+    $outlang = $_GET['outlang'];
 if (!isset($_GET['name'])) {
     http_response_code(400);
     echo "400 Bad Request - No Name Given";
     die();
 }
-$csv = file_get_contents('https://github.com/PokeAPI/pokeapi/raw/master/data/v2/csv/pokemon_species_names.csv');
-$pokemons = csv2json($csv);
-$pokemonFound = false;
 $reply = '';
-switch ($language) {
-    case 1:
+switch ($outlang) {
+    case 1: // Japanese Hiragana/Katakana
         $reply = "あなたのポケモンは見つかりませんでした!";
         break;
-    case 2:
+    case 2: // Japanese Rōmaji
         $reply = "Anata no pokemon wa mitsukarimasendeshita!";
         break;
-    case 3:
+    case 3: // Korean
         $reply = "당신의 포켓몬을 찾을 수 없습니다!";
         break;
-    case 4:
+    case 4: // Chinese (traditional)
         $reply = "你的小精灵找不到了！";
         break;
-    case 5:
+    case 5: // French
         $reply = "Ton Pokemon n'a pas pu être trouvé !";
         break;
-    case 6:
+    case 6: // German
         $reply = "Dein Pokemon konnte nicht gefunden werden!";
         break;
-    case 7:
+    case 7: // Spanish
         $reply = "¡Tu Pokemon no ha podido ser encontrado!";
         break;
-    case 8:
+    case 8: // Italian
         $reply = "Il tuo Pokemon non è stato trovato!";
         break;
-    case 10:
+    case 10: // Czech
         $reply = "Vašeho Pokémona se nepodařilo najít!";
         break;
-    default: // 9
+    case 11: // Japanese Hiragana/Katakana
+        $reply = "あなたのポケモンは見つかりませんでした!";
+        break;
+    case 12: // Chinese (simplified)
+        $reply = "找不到你的宝可梦！";
+        break;
+    case 13: // (Brazilian) Portuguese
+        $reply = "Seu Pokémon não foi encontrado!";
+        break;
+    default: // 9 // English
         $reply = "Your Pokemon couldn't be found!";
         break;
 }
-$pokemons = array_values($pokemons);
+$csv = file_get_contents('https://github.com/PokeAPI/pokeapi/raw/master/data/v2/csv/pokemon_species_names.csv');
+$pokemons = array_values(csv2json($csv));
+$pokemonFound = false;
 for ($i = 0; $i < count($pokemons); $i++) {
     if ($pokemonFound)
         break;
@@ -70,38 +278,8 @@ for ($i = 0; $i < count($pokemons); $i++) {
     if (strtolower($pokemons[$i]['name']) == strtolower($_GET['name'])) {
         for ($j = 0; $j < count($pokemons); $j++) {
             if ($pokemons[$i]['pokemon_species_id'] == $pokemons[$j]['pokemon_species_id'] && intval($pokemons[$j]['local_language_id']) == $language) {
-                switch ($language) {
-                    case 1:
-                        $reply = sprintf("あなたのポケモンの日本語名は \"%s\" です。", $pokemons[$j]['name']);
-                        break;
-                    case 2:
-                        $reply = sprintf("Anata no pokemon no nihongo-mei wa \"%s\" desu.", $pokemons[$j]['name']);
-                        break;
-                    case 3:
-                        $reply = sprintf("포켓몬의 한국 이름은 \"%s\" 입니다.", $pokemons[$j]['name']);
-                        break;
-                    case 4:
-                        $reply = sprintf("你的小精灵的中文名字是 \"%s\"。", $pokemons[$j]['name']);
-                        break;
-                    case 5:
-                        $reply = sprintf("Le nom français de votre pokémon est \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 6:
-                        $reply = sprintf("Der deutsche Name deines Pokemons lautet \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 7:
-                        $reply = sprintf("El nombre en español de tu pokemon es \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 8:
-                        $reply = sprintf("Il nome italiano del tuo pokemon è \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 10:
-                        $reply = sprintf("České jméno vašeho pokémona je \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    default: // 9
-                        $reply = sprintf("The english name of your pokemon is \"%s\".", $pokemons[$j]['name']);
-                        break;
-                }
+                $reply = sprintf($nameFoundPatterns[$language][$outlang], $pokemons[$j]['name']);
+                $pokemonFound = true;
             }
         }
     }
