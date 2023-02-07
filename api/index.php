cda25918c6f15aae48c6442e126f3bfbc47bf7f1
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-02-07 12:14:13 +0100

    Added a few languages
    TODO: Continue after lunch break

--- api/index.php
+++ api/index.php
@@ -1,4 +1,126 @@
 <?php
+$nameFoundPatterns = [
+    "1" => [ // Japanese Hiragana/Katakana Pokémon
+        "1" => "あなたのポケモンの日本語名は「%s」です。",
+        "2" => "Anata no pokemon no nihongo wa \"%s\" desu.",
+        "3" => "포켓몬의 일본어이름은 \"%s\" 입니다.",
+        "4" => "您的神奇寶貝的日語名稱是 \"%s\"。",
+        "5" => "Le nom japonais de votre Pokémon est \"%s\".",
+        "6" => "Der japanische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre japonés de tu Pokémon es \"%s\".",
+        "8" => "Il nome giapponese del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Japanese name is \"%s\".",
+        "10" => "Japonské jméno vašeho Pokémona je \"%s\"."
+    ],
+    "2" => [ // Japanese Rōmaji Pokémon
+        "1" => "あなたのポケモンの日本語名は「%s」です。",
+        "2" => "Anata no pokemon no nihongo wa \"%s\" desu.",
+        "3" => "포켓몬의 일본어이름은 \"%s\" 입니다.",
+        "4" => "您的神奇寶貝的日語名稱是 \"%s\"。",
+        "5" => "Le nom japonais de votre Pokémon est \"%s\".",
+        "6" => "Der japanische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre japonés de tu Pokémon es \"%s\".",
+        "8" => "Il nome giapponese del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Japanese name is \"%s\".",
+        "10" => "Japonské jméno vašeho Pokémona je \"%s\"."
+    ],
+    "3" => [ // Korean Pokémon
+        "1" => "あなたのポケモンの韓国名は「%s」です。",
+        "2" => "Anata no pokemon no Kankoku-mei wa \"%s\" desu.",
+        "3" => "당신의 포켓몬의 한국 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的韓國名字是 \"%s\"。",
+        "5" => "Le nom coréen de votre Pokémon est \"%s\".",
+        "6" => "Der koreanische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre coreano de tu Pokémon es \"%s\".",
+        "8" => "Il nome coreano del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Korean name is \"%s\".",
+        "10" => "Korejské jméno vašeho pokémona je \"%s\"."
+    ],
+    "4" => [ // Chinese (traditional) Pokémon
+        "1" => "あなたのポケモンの中国名は「%s」です。",
+        "2" => "Anata no pokemon no Chūgoku-mei wa \"%s\" desu.",
+        "3" => "포켓몬의 중국 이름은 \"%s\" 입니다.",
+        "4" => "你的寶可夢的中文名字是 \"%s\"。",
+        "5" => "Le nom chinois de votre Pokémon est \"%s\".",
+        "6" => "Der chinesische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre chino de tu Pokémon es \"%s\".",
+        "8" => "Il nome cinese del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Chinese name is \"%s\".",
+        "10" => "Čínské jméno vašeho pokémona je \"%s\"."
+    ],
+    "5" => [ // French Pokémon
+        "1" => "あなたのポケモンのフランス語名は「%s」です。",
+        "2" => "Anata no pokemon no furansugo-mei wa \"%s\" desu.",
+        "3" => "포켓몬의 프랑스어 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的法語名字是 \"%s\"。",
+        "5" => "Le nom français de votre Pokémon est \"%s\".",
+        "6" => "Der französische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre francés de tu Pokémon es \"%s\".",
+        "8" => "Il nome francese del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's French name is \"%s\".",
+        "10" => "Francouzské jméno vašeho Pokémona je \"%s\"."
+    ],
+    "6" => [ // German Pokémon
+        "1" => "あなたのポケモンのドイツ語名は「%s」です。",
+        "2" => "Anata no pokemon no doitsugo-mei wa \"%s\" desu.",
+        "3" => "포켓몬의 독일어 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的德語名字是 \"%s\"。",
+        "5" => "Le nom allemand de votre Pokémon est \"%s\".",
+        "6" => "Der deutsche Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre alemán de tu Pokémon es \"%s\".",
+        "8" => "Il nome tedesco del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's German name is \"%s\".",
+        "10" => "Německé jméno vašeho pokémona je \"%s\"."
+    ],
+    "7" => [ // Spanish Pokémon
+        "1" => "あなたのポケモンのスペイン語名は「%s」です。",
+        "2" => "Anata no pokemon no Supeingo-mei wa \"%s\" desu.",
+        "3" => "포켓몬의 스페인어 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的西班牙名字是 \"%s\"。",
+        "5" => "Le nom espagnol de votre Pokémon est \"%s\".",
+        "6" => "Der spanische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre en español de tu Pokémon es \"%s\".",
+        "8" => "Il nome spagnolo del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Spanish name is \"%s\".",
+        "10" => "Španělské jméno vašeho Pokémona je \"%s\"."
+    ],
+    "8" => [ // Italian Pokémon
+        "1" => "あなたのポケモンのイタリア語名は「%s」です。",
+        "2" => "Anata no pokemon no Itaria katarina wa \"%s\" desu.",
+        "3" => "포켓몬의 이탈리아어 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的意大利名字是 \"%s\"。",
+        "5" => "Le nom italien de votre Pokémon est \"%s\".",
+        "6" => "Der italienische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre italiano de tu Pokémon es \"%s\".",
+        "8" => "Il nome italiano del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Italian name is \"%s\".",
+        "10" => "Italské jméno vašeho Pokémona je \"%s\"."
+    ],
+    "9" => [ // English Pokémon
+        "1" => "あなたのポケモンの英語名は「%s」です。",
+        "2" => "Anata no pokemon no eigo-mei wa \"%s\" desu.",
+        "3" => "포켓몬의 영어 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的英文名字是 \"%s\"。",
+        "5" => "Le nom anglais de votre Pokémon est \"%s\".",
+        "6" => "Der englische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre en inglés de tu Pokémon es \"%s\".",
+        "8" => "Il nome inglese del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's English name is \"%s\".",
+        "10" => "Anglické jméno vašeho Pokémona je \"%s\"."
+    ],
+    "10" => [ // Czech Pokémon
+        "1" => "あなたのポケモンのチェコ語名は「%s」です。",
+        "2" => "Anata no pokemon no Cheko katarina wa \"%s\" desu.",
+        "3" => "포켓몬의 체코 이름은 \"%s\" 입니다.",
+        "4" => "你的口袋妖怪的捷克名字是 \"%s\"。",
+        "5" => "Le nom tchèque de votre Pokémon est \"%s\".",
+        "6" => "Der tschechische Name deines Pokemons lautet \"%s\".",
+        "7" => "El nombre checo de tu Pokémon es \"%s\".",
+        "8" => "Il nome ceco del tuo Pokemon è \"%s\".",
+        "9" => "Your Pokemon's Czech name is \"%s\".",
+        "10" => "Váš pokémon se česky jmenuje \"%s\"."
+    ]
+];
 function csv2json($csv) {
     $data = array_map("str_getcsv", explode("\n", $csv));
     $columns = $data[0];
