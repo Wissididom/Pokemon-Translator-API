bb18febe607a93926965f31573e7bab6d5ebbf58
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-02-07 14:18:09 +0100

    Use language dictionary

--- api/index.php
+++ api/index.php
@@ -227,34 +227,43 @@ if (!isset($_GET['name'])) {
 }
 $reply = '';
 switch ($outlang) {
-    case 1:
+    case 1: // Japanese Hiragana/Katakana
         $reply = "あなたのポケモンは見つかりませんでした!";
         break;
-    case 2:
+    case 2: // Japanese Rōmaji
         $reply = "Anata no pokemon wa mitsukarimasendeshita!";
         break;
-    case 3:
+    case 3: // Korean
         $reply = "당신의 포켓몬을 찾을 수 없습니다!";
         break;
-    case 4:
+    case 4: // Chinese (traditional)
         $reply = "你的小精灵找不到了！";
         break;
-    case 5:
+    case 5: // French
         $reply = "Ton Pokemon n'a pas pu être trouvé !";
         break;
-    case 6:
+    case 6: // German
         $reply = "Dein Pokemon konnte nicht gefunden werden!";
         break;
-    case 7:
+    case 7: // Spanish
         $reply = "¡Tu Pokemon no ha podido ser encontrado!";
         break;
-    case 8:
+    case 8: // Italian
         $reply = "Il tuo Pokemon non è stato trovato!";
         break;
-    case 10:
+    case 10: // Czech
         $reply = "Vašeho Pokémona se nepodařilo najít!";
         break;
-    default: // 9
+    case 11: // Japanese Hiragana/Katakana
+        $reply = "Vašeho Pokémona se nepodařilo najít!";
+        break;
+    case 12: // Chinese (simplified)
+        $reply = "Vašeho Pokémona se nepodařilo najít!";
+        break;
+    case 13: // (Brazilian) Portuguese
+        $reply = "Vašeho Pokémona se nepodařilo najít!";
+        break;
+    default: // 9 // English
         $reply = "Your Pokemon couldn't be found!";
         break;
 }
@@ -269,38 +278,7 @@ for ($i = 0; $i < count($pokemons); $i++) {
     if (strtolower($pokemons[$i]['name']) == strtolower($_GET['name'])) {
         for ($j = 0; $j < count($pokemons); $j++) {
             if ($pokemons[$i]['pokemon_species_id'] == $pokemons[$j]['pokemon_species_id'] && intval($pokemons[$j]['local_language_id']) == $language) {
-                switch ($outlang) {
-                    case 1:
-                        $reply = sprintf("あなたのポケモンの日本語名は \"%s\" です。", $pokemons[$j]['name']);
-                        break;
-                    case 2:
-                        $reply = sprintf("Anata no pokemon no nihongo-mei wa \"%s\" desu.", $pokemons[$j]['name']);
-                        break;
-                    case 3:
-                        $reply = sprintf("포켓몬의 한국 이름은 \"%s\" 입니다.", $pokemons[$j]['name']);
-                        break;
-                    case 4:
-                        $reply = sprintf("你的小精灵的中文名字是 \"%s\"。", $pokemons[$j]['name']);
-                        break;
-                    case 5:
-                        $reply = sprintf("Le nom français de votre pokémon est \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 6:
-                        $reply = sprintf("Der deutsche Name deines Pokemons lautet \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 7:
-                        $reply = sprintf("El nombre en español de tu pokemon es \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 8:
-                        $reply = sprintf("Il nome italiano del tuo pokemon è \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    case 10:
-                        $reply = sprintf("České jméno vašeho pokémona je \"%s\".", $pokemons[$j]['name']);
-                        break;
-                    default: // 9
-                        $reply = sprintf("The english name of your pokemon is \"%s\".", $pokemons[$j]['name']);
-                        break;
-                }
+                $reply = sprintf($nameFoundPatterns[$language][$outlang], $pokemons[$j]['name']);
             }
         }
     }
